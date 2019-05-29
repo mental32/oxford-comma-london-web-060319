f97123a194fb923214e00607a742e3d96f6c34cf
beatific-revision-5216 <ivanov_chris@yahoo.com>
2019-05-29 04:38:58 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,6 @@ def oxford_comma(array)
   if array.size == 2
     array.join 'and'
   else
-    arra.first(array.size - 1).join(', ') + "and #{array.last}"
+    array.first(array.size - 1).join(', ') + "and #{array.last}"
   end
 end
